7c5bfdeee363512ab22814f32cef3584e3e30417
slp <slp@qq.com>
2020-09-07 17:02:37 +0800

    在家继续开发签到功能

---
+++ SignIn.java
@@ -0,0 +1,12 @@
+public class SignIn{
+	private int id;
+	private Strin age;
+	public void sign(){
+		System.out.println("在家开发的代码");
+
+		}
+		public void sign(int id){
+				System.out.println("在家开发的代码");
+
+		}
+	}
